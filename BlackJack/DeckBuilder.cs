a8804826b98acbcbe2bc7f9bbe0f9a53cbe1a96e
Michael Anderson <Micmail566@gmail.com>
2017-01-22 20:07:34 -0500

    need to assign values to cards

--- BlackJack/DeckBuilder.cs
+++ BlackJack/DeckBuilder.cs
@@ -9,21 +9,28 @@ using System.IO;
 
 namespace BlackJack
 {
+   
     public class DeckBuilder : Card
     {
+        
+        public int DynamicValue;
+        
 
+        public List<string> Deck = new List<string>();
         public string BuildDeck()
         {
 
             Card newCard = new Card();
             int dynamicValue = 2;
-            var deck = new List<String>();
+            var deck = Deck;
+            
             for (int i = 1; i < 15; i++)
             {
                 newCard.cardValue = dynamicValue.ToString();
                 if (dynamicValue < 10)
                 { //2-9
                     dynamicValue++;
+
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[2]} ");
@@ -33,6 +40,7 @@ namespace BlackJack
                 else if (dynamicValue == 10 && i == 10)
                 { //10s
                     dynamicValue = 10;
+                  
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.cardValue} of {newCard.suit[2]} ");
@@ -42,6 +50,7 @@ namespace BlackJack
                 else if (dynamicValue == 10 && i == 11)
                 { //Jacks
                     dynamicValue = 10;
+                   
                     deck.Add($"The {newCard.face[0]} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.face[0]} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.face[0]} of {newCard.suit[2]} ");
@@ -50,6 +59,7 @@ namespace BlackJack
                 else if (dynamicValue == 10 && i == 12)
                 { //Queens
                     dynamicValue = 10;
+                  
                     deck.Add($"The {newCard.face[1]} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.face[1]} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.face[1]} of {newCard.suit[2]} ");
@@ -58,6 +68,7 @@ namespace BlackJack
                 else if (dynamicValue == 10 && i == 13)
                 { //Kings
                     dynamicValue = 10;
+                   
                     deck.Add($"The {newCard.face[2]} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.face[2]} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.face[2]} of {newCard.suit[2]} ");
@@ -66,13 +77,14 @@ namespace BlackJack
                 else if (dynamicValue == 10 && i == 14)
                 { //Ace
                     dynamicValue = 11;
+                   
                     deck.Add($"The {newCard.face[3]} of {newCard.suit[0]} ");
                     deck.Add($"The {newCard.face[3]} of {newCard.suit[1]} ");
                     deck.Add($"The {newCard.face[3]} of {newCard.suit[2]} ");
                     deck.Add($"The {newCard.face[3]} of {newCard.suit[3]} ");
                 }
 
-
+              
             }
             Random rnd = new Random();
             List<string> rndDeck = deck.OrderBy(x => rnd.Next()).ToList();
@@ -80,6 +92,7 @@ namespace BlackJack
             File.WriteAllLines(deckfile, rndDeck);
             string pulledCard = File.ReadLines(@"..\..\Deck.txt").First();
             return pulledCard;
+           
 
 
 
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
+    public enum State
+    {
+        two = 2,
+        three = 3,
+        four = 4,
+        five = 5,
+        six = 6,
+        seven = 7,
+        eight = 8,
+        nine = 9,
+        ten = 10,
+        faceCard = 10,
+        Ace = 11
+    }
+
     class Program
     {
+        
         static void Main(string[] args)
         {
+            State currentState;
             DeckBuilder drawcard = new DeckBuilder();
+           
             string reshuffle = drawcard.BuildDeck();
             //^^ Resuffles Deck
             Console.WriteLine("Welcome To BlackJack");
@@ -56,11 +74,28 @@ namespace BlackJack
                 hand.Player1Hand.Add(newCard);
                 Console.WriteLine("Do You Want To Hit?");
                 Console.WriteLine("Your Cards Are");
-                Console.WriteLine();
+              
                 foreach (var card in displayHand)
                 {
+                   
+                    int value3;
+                    int value4;
+
+                    int count = card.Count(x => x == '2');
+                    int value2;
+                    if (count == 1)
+                    value2 = 2
+                    int count2 = card.Count(x => x == '3');
+                    if (count2 == 1)
+                        value3 = 3;
+                    int count3 = card.Count(x => x == '4');
+                    if (count3 == 1)
+                        value3 = 4;
+
                     Console.WriteLine(card);
+                    
                 }
+       
                 askForHit = Console.ReadLine();
              
 
